299cdad50df8f3d74e38f3e69a2ead2eca61dec7
BlueWatermalone <71526538+BlueWatermalone@users.noreply.github.com>
2021-05-11 20:35:05 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX = 0;
+noseY = 0;
+
 function preload() {
+    clown_nose = loadImage("https://i.postimg.cc/xTN6R25y/Clown-nose-large.png")
 }
 
 function setup() {
@@ -18,14 +22,16 @@ function modelLoaded() {
 
 function gotPoses(results) {
     if(results.length > 0) {
-        console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 32;
+        noseY = results[0].pose.nose.y - 30;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
 }
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 70, 70);
 }
 
 function takeSnapshot() {
